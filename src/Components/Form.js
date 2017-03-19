ddbbf65155b0afdf3cf0806f75cba0af3f6f752a
Ninan <jninan@thoughtworks.com>
2017-03-19 12:38:06 +0530

    2 [Ninan/ReactDocs] Remove text on first click only.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,10 +5,23 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state =
-            {value: ''};
+            {
+                value: 'Enter your name',
+                isClicked: false
+            };
 
+        this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleClick(event) {
+        if (!this.state.isClicked) {
+            this.setState({
+                value: '',
+                isClicked: true
+            });
+        }
     }
 
     handleChange(event) {
@@ -26,7 +39,7 @@ class Form extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label> Name: </label>
-                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <textarea type="text" value={this.state.value} onChange={this.handleChange} onClick={this.handleClick}/>
                 <input type="submit" value="Submit"/>
             </form>
         );
